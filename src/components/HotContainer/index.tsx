423d806de7620f2126448394437713879e20ee06
baiwumm <baiwumm@foxmail.com>
2024-05-15 16:53:43 +0800

    perf(HotContainer): 去掉冗余的代码

--- src/components/HotContainer/index.tsx
+++ src/components/HotContainer/index.tsx
@@ -2,37 +2,15 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-05-10 17:06:14
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-05-15 13:58:39
+ * @LastEditTime: 2024-05-15 16:51:01
  * @Description: 热榜卡片
  */
-'use client';
-
-import { useState } from 'react';
-
-import { useLocalStorageState } from 'ahooks';
-
-import { LOCAL_KEY } from '@/utils/enum';
-import type { HotListConfig, HotTypes } from '@/utils/types';
-
 import HotCard from './HotCard';
 
 import { hotCardConfig } from '@/utils';
 
 const HotContainer = () => {
-  // 不显示的榜单列表
-  const [hiddenHotList] = useLocalStorageState<HotTypes[]>(LOCAL_KEY.HOTHIDDEN, {
-    defaultValue: [],
-  });
-  /**
-   * @description: 过滤掉不显示的热榜
-   */
-  const filterHiddenHot = (): HotListConfig[] => {
-    return hiddenHotList?.length
-      ? hotCardConfig.filter((config: HotListConfig) => !hiddenHotList.includes(config.value))
-      : hotCardConfig;
-  };
-  const [hotConfig] = useState<HotListConfig[]>(filterHiddenHot());
-  return hotConfig.map((config, index) => (
+  return hotCardConfig.map((config, index) => (
     <div
       key={config.value}
       className="animate-[showCard_0.5s_ease-in-out_forwards] opacity-0"
