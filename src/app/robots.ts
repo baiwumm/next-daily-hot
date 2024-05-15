975f9cd08dffd10047cb4cc61f81834faf822b27
baiwumm <baiwumm@foxmail.com>
2024-05-15 17:06:31 +0800

    feat(app): 新增 robots 文件

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'http://localhost:6688/sitemap.xml',
+  };
+}
