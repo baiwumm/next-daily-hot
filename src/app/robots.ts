a28319bd80baa066ac044f65bd6b9d7fe17e0d4a
baiwumm <baiwumm@foxmail.com>
2024-05-17 16:16:46 +0800

    perf: 修改 robots sitemap 地址

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'http://localhost:6688/sitemap.xml',
+    sitemap: `${process.env.SITE_URL}sitemap.xml`,
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://hot.baiwumm.com',
+      url: process.env.SITE_URL || '',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
