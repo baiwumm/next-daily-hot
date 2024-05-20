3270714c09c155c273a403908f4ea6166c51ea79
baiwumm <baiwumm@foxmail.com>
2024-05-20 14:59:50 +0800

    fix: 修复 MICROSOFT_CLARITY 变量引发的报错

--- src/components/Analytics/MicrosoftClarity.tsx
+++ src/components/Analytics/MicrosoftClarity.tsx
@@ -2,7 +2,7 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-05-20 09:33:01
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-05-20 09:55:10
+ * @LastEditTime: 2024-05-20 14:59:20
  * @Description: 微软统计
  */
 import Script from 'next/script';
@@ -12,7 +12,7 @@ const MicrosoftClarity = () => {
     <Script
       id="microsoft-clarity"
       dangerouslySetInnerHTML={{
-        __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script",${process.env.MICROSOFT_CLARITY});`,
+        __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","${process.env.MICROSOFT_CLARITY}");`,
       }}
     />
   ) : null;
