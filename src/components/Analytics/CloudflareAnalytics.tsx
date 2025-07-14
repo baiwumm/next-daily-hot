a1cca61985e3465c1c43088405deae3e67cf32ed
白雾茫茫丶 <baiwumm@foxmail.com>
2025-07-14 13:50:31 +0800

    feat: 更改 Cloudflare 统计代码

--- src/components/Analytics/CloudflareAnalytics.tsx
+++ src/components/Analytics/CloudflareAnalytics.tsx
@@ -8,8 +8,8 @@
 import Script from 'next/script';
 
 const CloudflareAnalytics = () => {
-  return process.env.NODE_ENV === 'production' ? (
-    <Script src="https://analytics.baiwumm.com/tracker.min.js" data-website-id="Hello-Daily-Hot" />
+  return process.env.CLOUDFLARE_ANALYTICS && process.env.NODE_ENV === 'production' ? (
+    <Script src="https://analytics.baiwumm.com/tracker.min.js" data-website-id={process.env.CLOUDFLARE_ANALYTICS} />
   ) : null;
 };
 export default CloudflareAnalytics;
